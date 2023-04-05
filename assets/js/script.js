956c00aaff978446b781d6ef5d3703b5673a7d11
dev-rashidul <rashidul.contact@gmail.com>
2023-04-05 21:52:17 +0600

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,29 +13,3 @@ const dropdown = document.querySelector(".dropdown")
 dropdownIcon.addEventListener("click", function(){
   dropdown.classList.toggle('active')
 })
-
-// JavaScript for Contact page event Slider
-
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  margin: 10,
-  responsiveClass: true,
-  responsive: {
-    0: {
-      items: 1,
-      nav: true,
-    },
-    700: {
-      items: 2,
-      nav: true,
-    },
-    1000: {
-      items: 3,
-      nav: true,
-    },
-  },
-  navText: [
-    '<i class="fa-solid fa-chevron-left"></i>',
-    '<i class="fa-solid fa-chevron-right"></i>',
-  ],
-});
